71cae963db47a5c1a6bd5c1adb3e675da94b2e28
yagol2020 <yhw_yagol@yeah.net>
2021-11-17 21:28:58 +0800

    feat: 打包为jar包完成

--- src/main/java/gui/designer/MainGui.java
+++ src/main/java/gui/designer/MainGui.java
@@ -99,7 +99,6 @@ public class MainGui {
                 } else {
                     return null;
                 }
-
             }
         });
         searchQueryInput.addKeyListener(new KeyAdapter() {
